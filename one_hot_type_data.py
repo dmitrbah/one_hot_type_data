5c454df84e46f2eaf68d83a9897e9cb5786470f4
Dmitry <dmitrbah@mail.ru>
2024-02-06 19:17:13 +0300

    Added one hot data modification logic

--- one_hot_type_data.py
+++ one_hot_type_data.py
@@ -8,5 +8,19 @@ import matplotlib.pyplot as plt
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
+
+robot_lst = [0]*20
+human_lst = [0]*20
+for i in range(len(lst)):
+    if lst[i] == 'robot':
+        robot_lst[i] = 1
+        human_lst[i] = 0
+    else:
+        robot_lst[i] = 0
+        human_lst[i] = 1        
+
 data = pd.DataFrame({'whoAmI':lst})
+data_one_hot = pd.DataFrame({'robot':robot_lst, 'human': human_lst})
 print(data.head(n=20))
+print(data_one_hot.head(n=20))
+
